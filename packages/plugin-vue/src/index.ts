754553ea5b630623a8b32d74f792c281ef37b5f8
ʀᴀʏ <i@mk1.io>
2023-05-12 15:33:25 +0800

    tag

--- packages/plugin-vue/src/index.ts
+++ packages/plugin-vue/src/index.ts
@@ -53,7 +53,7 @@ export interface Options {
   customElement?: boolean | string | RegExp | (string | RegExp)[]
 
   /**
-   * Enable Vue reactivity transform (experimental).
+   * Enable Vue reactivity transform (deprecated).
    * https://vuejs.org/guide/extras/reactivity-transform.html
    * - `true`: transform will be enabled for all vue,js(x),ts(x) files except
    *           those inside node_modules
@@ -61,6 +61,7 @@ export interface Options {
    *                      node_modules, so specify directories if necessary)
    * - `false`: disable in all cases
    *
+   * @deprecated
    * @default false
    */
   reactivityTransform?: boolean | string | RegExp | (string | RegExp)[]
@@ -68,6 +69,8 @@ export interface Options {
   /**
    * Enable `defineModel` macro (experimental).
    * https://github.com/vuejs/rfcs/discussions/503
+   *
+   * @experimental
    * @default false
    */
   defineModel?: boolean
@@ -75,6 +78,8 @@ export interface Options {
   /**
    * Enable reactive props destructure (experimental).
    * https://github.com/vuejs/rfcs/discussions/502
+   *
+   * @experimental
    * @default false
    */
   propsDestructure?: boolean
