d7ee0059e50e5503e01ed5e44786aac71e163c66
a1mer <onlylovesnow14@gmail.com>
2023-08-04 09:23:17 +0800

    fix: vue-ssr external config merge

--- playground/ssr-vue/vite.config.noexternal.js
+++ playground/ssr-vue/vite.config.noexternal.js
@@ -1,25 +1,27 @@
-import config from './vite.config.js'
-/**
- * @type {import('vite').UserConfig}
- */
-export default Object.assign(config, {
-  ssr: {
-    noExternal: /./,
-  },
-  resolve: {
-    // necessary because vue.ssrUtils is only exported on cjs modules
-    alias: [
-      {
-        find: '@vue/runtime-dom',
-        replacement: '@vue/runtime-dom/dist/runtime-dom.cjs.js',
-      },
-      {
-        find: '@vue/runtime-core',
-        replacement: '@vue/runtime-core/dist/runtime-core.cjs.js',
-      },
-    ],
-  },
-  optimizeDeps: {
-    exclude: ['@vitejs/test-example-external-component'],
-  },
+import { defineConfig } from 'vite'
+import createConfig from './vite.config.js'
+
+export default defineConfig((env) => {
+  const config = createConfig(env)
+  return Object.assign(config, {
+    ssr: {
+      noExternal: /./,
+    },
+    resolve: {
+      // necessary because vue.ssrUtils is only exported on cjs modules
+      alias: [
+        {
+          find: '@vue/runtime-dom',
+          replacement: '@vue/runtime-dom/dist/runtime-dom.cjs.js',
+        },
+        {
+          find: '@vue/runtime-core',
+          replacement: '@vue/runtime-core/dist/runtime-core.cjs.js',
+        },
+      ],
+    },
+    optimizeDeps: {
+      exclude: ['@vitejs/test-example-external-component'],
+    },
+  })
 })
