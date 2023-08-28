1637a51a06f9cc7e23d92aa3379b0cd9944f1ef5
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-08-29 01:37:29 +0800

    fix: types

--- scripts/releaseUtils.ts
+++ scripts/releaseUtils.ts
@@ -70,7 +70,7 @@ export function getPackageInfo(pkgName: string): {
 export async function run(
   bin: string,
   args: string[],
-  opts: ExecaOptions<string> = {},
+  opts: ExecaOptions = {},
 ): Promise<ExecaReturnValue<string>> {
   return execa(bin, args, { stdio: 'inherit', ...opts })
 }
@@ -78,7 +78,7 @@ export async function run(
 export async function dryRun(
   bin: string,
   args: string[],
-  opts?: ExecaOptions<string>,
+  opts?: ExecaOptions,
 ): Promise<void> {
   return console.log(
     colors.blue(`[dryrun] ${bin} ${args.join(' ')}`),
