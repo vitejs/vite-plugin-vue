7c8b8420c6d607581c7144f85fe0e3897d09f01a
NateScarlet <NateScarlet@Gmail.com>
2024-01-09 11:26:21 +0800

    docs: update comment

--- playground/vue/HmrTypeScopeFile.d.ts
+++ playground/vue/HmrTypeScopeFile.d.ts
@@ -1,4 +1,4 @@
-// this file will become a TypeScope
+// this file will become a TypeScope through option `script.globalTypeFiles`
 // which has a circular reference on `TypeScope._ownerScope`
 // will cause all test hook timeout if not handled (issue 325)
 
