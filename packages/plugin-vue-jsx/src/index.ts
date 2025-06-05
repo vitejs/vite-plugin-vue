e25f7d20776d495a1c96dfd466742b3f8bbdb810
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-06-05 14:13:05 +0900

    refactor: use crypto.hash

--- packages/plugin-vue-jsx/src/index.ts
+++ packages/plugin-vue-jsx/src/index.ts
@@ -328,17 +328,8 @@ function isDefineComponentCall(
   )
 }
 
-const hash =
-  // eslint-disable-next-line n/no-unsupported-features/node-builtins -- crypto.hash is supported in Node 21.7.0+, 20.12.0+
-  crypto.hash ??
-  ((
-    algorithm: string,
-    data: crypto.BinaryLike,
-    outputEncoding: crypto.BinaryToTextEncoding,
-  ) => crypto.createHash(algorithm).update(data).digest(outputEncoding))
-
 function getHash(text: string) {
-  return hash('sha256', text, 'hex').substring(0, 8)
+  return crypto.hash('sha256', text, 'hex').substring(0, 8)
 }
 
 export default vueJsxPlugin
