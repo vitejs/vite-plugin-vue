5de85f6a15d7ca98e2823d1c0f5799717a077e21
翠 <green@sapphi.red>
2025-06-16 21:44:19 +0900

    refactor: always use `crypto.hash` (#606)

--- packages/plugin-vue/src/utils/descriptorCache.ts
+++ packages/plugin-vue/src/utils/descriptorCache.ts
@@ -148,15 +148,6 @@ export function setSrcDescriptor(
   cache.set(filename, entry)
 }
 
-const hash =
-  // eslint-disable-next-line n/no-unsupported-features/node-builtins -- crypto.hash is supported in Node 21.7.0+, 20.12.0+
-  crypto.hash ??
-  ((
-    algorithm: string,
-    data: crypto.BinaryLike,
-    outputEncoding: crypto.BinaryToTextEncoding,
-  ) => crypto.createHash(algorithm).update(data).digest(outputEncoding))
-
 function getHash(text: string): string {
-  return hash('sha256', text, 'hex').substring(0, 8)
+  return crypto.hash('sha256', text, 'hex').substring(0, 8)
 }
