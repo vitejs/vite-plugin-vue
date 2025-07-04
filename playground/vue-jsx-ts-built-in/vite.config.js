0960987289f9d312bf08d321fdd53bb0db6a0463
madeofsun <rettou74@gmail.com>
2025-07-05 02:22:15 +0300

    feat(vue-jsx): allow esbuild to perform ts transformation

--- playground/vue-jsx-ts-built-in/vite.config.js
+++ playground/vue-jsx-ts-built-in/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vueJsxPlugin from '@vitejs/plugin-vue-jsx'
 import vuePlugin from '@vitejs/plugin-vue'
-import babelPluginSyntaxTypescript from '@babel/plugin-syntax-decorators'
+import babelPluginSyntaxDecorators from '@babel/plugin-syntax-decorators'
 
 export default defineConfig({
   plugins: [
@@ -9,7 +9,7 @@ export default defineConfig({
       tsTransform: 'built-in',
       babelPlugins: [
         [
-          babelPluginSyntaxTypescript,
+          babelPluginSyntaxDecorators,
           // to test decorators we use only method decorators
           // they have the same syntax in 'legacy' and in '2023-11'
           { version: '2023-11' },
