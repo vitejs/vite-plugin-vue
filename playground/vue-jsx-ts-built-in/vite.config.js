3067aa3e512163a587a1e3c2f6c733826bad6dc8
madeofsun <rettou74@gmail.com>
2025-07-05 01:38:43 +0300

    feat(vue-jsx): allow esbuild to perform ts transformation

--- playground/vue-jsx-ts-built-in/vite.config.js
+++ playground/vue-jsx-ts-built-in/vite.config.js
@@ -5,7 +5,6 @@ import vuePlugin from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vueJsxPlugin({
-      include: [/\.tesx$/, /\.[jt]sx$/],
       tsTransform: 'built-in',
       babelPlugins: [
         // to tests decorators we use only method decorators
@@ -14,27 +13,6 @@ export default defineConfig({
       ],
     }),
     vuePlugin(),
-    {
-      name: 'jsx-query-plugin',
-      transform(code, id) {
-        if (id.includes('?query=true')) {
-          return `
-import { createVNode as _createVNode } from "vue";
-import { defineComponent, ref } from 'vue';
-export default defineComponent(() => {
-  const count = ref(6);
-
-  const inc = () => count.value++;
-
-  return () => _createVNode("button", {
-    "class": "jsx-with-query",
-    "onClick": inc
-  }, [count.value]);
-});
-`
-        }
-      },
-    },
   ],
   build: {
     // to make tests faster
